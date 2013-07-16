7588f5dd4f761ee2eeb58fa7fd1d7dfef80a4bf2
Tom Aratyn <tom@aratyn.name>
2013-07-16 15:28:50 -0400

    added a sql schema to the security demos.

---
+++ security/schema.sql
@@ -0,0 +1,8 @@
+delimiter $$
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
