2b9bd1c03487bbfff92d66d16c3a7f4a2872c59d
Tom Aratyn <tom@aratyn.name>
2013-07-18 16:20:03 -0400

    fixed sql files to use the normal delimiter.

--- php-database-objects/schema.sql
+++ php-database-objects/schema.sql
@@ -1,8 +1,7 @@
-delimiter $$
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(150) NOT NULL,
   `password` varchar(150) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
