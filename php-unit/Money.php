3c831db03852255c0e748cf92e76bac4f8597dfd
Tom Aratyn <tom@aratyn.name>
2013-07-23 15:13:27 -0400

    Added an equals method and improved unit tests.

--- php-unit/Money.php
+++ php-unit/Money.php
@@ -10,8 +10,12 @@ class Money {
         $this->currency = $currency;
     }
 
+    public function equals(Money $other) {
+        return $this->amount === $other->amount && $this->currency === $other->currency;
+    }
+
     public function times($multiplier) {
-        return new Money($this->amount * $multiplier, $this->currency);
+        return new Money((int)floor($this->amount * $multiplier), $this->currency);
     }
 
 }
--- php-unit/MoneyTest.php
+++ php-unit/MoneyTest.php
@@ -8,12 +8,23 @@ require_once "Money.php";
 
 class MoneyTest extends PHPUnit_Framework_TestCase {
 
+    public function testEquals() {
+        $oneDollar = new Money(1, "CAD");
+        $oneFranc = new Money(1, "CHF");
+        $twoDollars = new Money(2, "CAD");
+        $alsoTwoDollars = new Money(2, "CAD");
+        $this->assertTrue($twoDollars->equals($alsoTwoDollars));
+        $this->assertTrue($alsoTwoDollars->equals($twoDollars));
+        $this->assertFalse($oneDollar->equals($oneFranc));
+        $this->assertFalse($oneDollar->equals($twoDollars));
+    }
+
     public function testTimes() {
         $dollar = new Money(10);
         $tenDollars = new Money(20);
         $fiveDollars = new Money(5);
-        $this->assertEquals($tenDollars, $dollar->times(2));
-        $this->assertEquals($fiveDollars, $dollar->times(.5));
+        $this->assertTrue($tenDollars->equals($dollar->times(2)));
+        $this->assertTrue($fiveDollars->equals($dollar->times(.5)));
     }
 
     public function testConstructor() {
